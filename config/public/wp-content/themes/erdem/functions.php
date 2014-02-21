4bbba6a57126125035498d82d2ccff0290da33dc
Alex Vince <avince.digital@gmail.com>
2014-02-21 10:10:56 +0000

    Broken fucntions php

--- config/public/wp-content/themes/erdem/functions.php
+++ config/public/wp-content/themes/erdem/functions.php
@@ -166,9 +166,10 @@ add_filter( 'thermal_post_entity',  function($data, $post, $state) {
 
     var $get = $_GET['explore'];
 
+    error_log( $get );
 
-    if ( $get === 'explore' )
-    {
+    /*if ( $get === 'explore' )
+    {*/
 
 			$data->meta->collection_copy = get_post_meta( $post->ID, 'collection_copy');
 			$data->meta->collection_runway_active = get_post_meta( $post->ID, 'collection_runway_active');
@@ -188,7 +189,7 @@ add_filter( 'thermal_post_entity',  function($data, $post, $state) {
 			setmetaData($data, $post, 'collection_backstage');
 			$data->meta->collection_video = get_post_meta( $post->ID, 'collection_video');
 
-		} else {
+		/*} else {*/
 
 			// Stockists fields
 
@@ -222,7 +223,7 @@ add_filter( 'thermal_post_entity',  function($data, $post, $state) {
 			$data->meta->contact_telephone = get_post_meta( $post->ID, 'contact_telephone', true);
 			$data->meta->contact_email = get_post_meta( $post->ID, 'contact_email', true);
 
-		}
+		/*}*/
 
 	}
 
